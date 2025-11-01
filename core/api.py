a44aa82577a8f8302d9224212baaf577183732fe
Jennifer Andersson <jennifer_andersson90@hotmail.com>
2025-11-01 11:55:42 +0100

    Added sensor endpoints and tested in /api/docs

--- core/api.py
+++ core/api.py
@@ -2,13 +2,20 @@
 from ninja import Router
 from .models import Sensor
 from typing import List
+from .schemas import SensorCreate, SensorOut
 
 # Create a router to handle API endpoints related to sensors
 router = Router()
 
 # Endpoint to get a list of all sensors from the database
-@router.get("/sensors")
+@router.get("/sensors", response=List[SensorOut])
 def list_sensors(request):
     sensors = Sensor.objects.all()
-    return [{"id": s.id, "name": s.name, "type": s.type} for s in sensors]
+    return sensors
+
+# Create a new sensor using data from the request
+@router.post("/sensors", response=SensorOut)
+def create_sensor(request, data: SensorCreate):
+    sensor = Sensor.objects.create(**data.dict()) # Turns the data schema into fields when creating a new sensor
+    return sensor
 
