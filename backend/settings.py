abad8f8992d250e7b9db0182de48396efbdbcc74
Jennifer Andersson <jennifer_andersson90@hotmail.com>
2025-11-05 12:08:24 +0100

    Added Docker Compose setup with Postgres and verified migrations and seed command

--- backend/settings.py
+++ backend/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": os.environ.get("POSTGRES_DB", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.environ.get("POSTGRES_HOST", "db"),
+        "PORT": os.environ.get("POSTGRES_HOST", "5432"),
     }
 }
 
